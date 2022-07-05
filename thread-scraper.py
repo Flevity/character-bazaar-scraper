ad5a20b7e1af04ecae8c5d6c5fd647221b741680
Flevity <flevity@yandex.ru>
2022-07-05 16:44:23 +0300

    Added parser for skillboard url

--- thread-scraper.py
+++ thread-scraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 SITE = 'https://forums.eveonline.com/c/marketplace/character-bazaar/'
 thread_url = 'https://forums.eveonline.com/t/'
-excludes = ['wtb', 'sold', 'new skillboard', 'welcome to the character bazaar']
+excludes = ['wtb', 'sold', 'new skillboard', 'welcome to the character bazaar', 'private']
 scroll = 1000
 
 page = webdriver.Chrome()
@@ -21,4 +21,10 @@ thread_data = [{'id': thread.get_attribute('data-topic-id'),
                if all(string not in thread.find_element(By.CSS_SELECTOR, '.title.raw-link.raw-topic-link').text.lower()
                       for string in excludes)]
 
+for thread in thread_data:
+    page.get(thread_url + thread['id'])
+    urls = page.find_elements(By.CSS_SELECTOR, '#post_1 .cooked a')
+    for url in urls:
+        if 'skillboard.eveisesi.space' in url.get_attribute('href'):
+            thread['url'] = url.get_attribute('href')
 print(*thread_data, sep='\n')
