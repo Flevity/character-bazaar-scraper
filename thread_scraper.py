a4e8aa8f9171cb858b695c149859a4d2be2d3c78
Flevity <flevity@yandex.ru>
2022-07-16 08:04:36 +0300

    Table schema adjusted.

--- thread_scraper.py
+++ thread_scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import sqlite3
+import datetime
 
 SITE = 'https://forums.eveonline.com/c/marketplace/character-bazaar/'
 thread_url = 'https://forums.eveonline.com/t/'
@@ -22,6 +23,7 @@ while len(threads) < 200:
     threads = page.find_elements(By.CSS_SELECTOR, '.topic-list-item')
 thread_data = [{'id': thread.get_attribute('data-topic-id'),
                 'name': thread.find_element(By.CSS_SELECTOR, '.title.raw-link.raw-topic-link').text,
+                'last_update': datetime.datetime.now(),
                 'activity': thread.find_element(By.CSS_SELECTOR, '.relative-date').text} for thread in threads
                if all(string not in thread.find_element(By.CSS_SELECTOR, '.title.raw-link.raw-topic-link').text.lower()
                       for string in excludes)]
@@ -39,19 +41,24 @@ print(f'{len(thread_data)} skillboard urls found.')
 print(*thread_data, sep='\n')
 
 db_file = 'data.db'
-table_name = 'parser_data'
+table_name = 'threads'
 
 try:
     sqlite_connection = sqlite3.connect(db_file)
     cursor = sqlite_connection.cursor()
     print("DB created and connected.")
 
-    cursor.execute(f'CREATE TABLE if not exists {table_name} (id integer NOT NULL primary key, name text, activity '
-                   'text, url text);')
+    cursor.execute(f'CREATE TABLE if not exists {table_name} ('
+                   f'id integer primary key, '
+                   f'name text, '
+                   f'last_update timestamp, '
+                   f'activity text, '
+                   f'url text);')
 
-    cursor.executemany(f'insert into {table_name}(id, name, activity, url) values (:id, :name, :activity, :url) on '
-                       'conflict(id) DO UPDATE SET name = excluded.name, activity = excluded.activity, '
-                       'url = excluded.url', thread_data)
+    cursor.executemany(f'INSERT INTO {table_name}(id, name, last_update, activity, url) '
+                       f'values (:id, :name, :last_update, :activity, :url) '
+                       f'on conflict(id) DO UPDATE SET name = excluded.name, last_update = excluded.last_update, '
+                       f'activity = excluded.activity, url = excluded.url', thread_data)
 
     sqlite_connection.commit()
     print("Data pushed to DB.")
